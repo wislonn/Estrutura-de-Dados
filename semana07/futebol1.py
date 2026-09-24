eaf0ffc0c9ac927ab34a665f8c14bc7b23d52675
william martins rabenschlag <william.rabenschlag@amf.edu.br>
2026-09-24 16:26:04 -0300

    Update futebol1.py

--- semana07/futebol1.py
+++ semana07/futebol1.py
@@ -5,11 +5,6 @@ class No:
 
 
 
-
-
-
-
-
 def adicionar(lista, jogador):
     jogador = No(jogador)
     if lista == None:
@@ -19,6 +14,21 @@ def adicionar(lista, jogador):
     else:
         jogador.proximo = lista
         lista = jogador
+        return lista
+
+
+def final (lista, jogador):
+    novo = No(jogador)
+    aux = lista
+    if lista == None:
+        lista = jogador
+        return lista
+
+    else:
+        while aux.proximo != None:
+            aux = aux.proximo
+
+        aux.proximo = novo
         return lista
 
 def mostrar(jogador):
@@ -31,6 +41,15 @@ def mostrar(jogador):
         jogador = jogador.proximo
 
 
+def percorrer(atual, jogador):
+    atual = No(jogador)
+    print(atual.jogador)
+    atual = atual.proximo
+    return atual
+
+
+
+
 def menu():
     print("1 - adicionar jogador")
     print("2 - mostrar lista")
@@ -39,7 +58,7 @@ def menu():
 def main():
     lista = None
     opc = 0
-    while opc != 3:
+    while opc != 5:
         menu()
         opc = int(input("qual a opção? "))
         if opc == 1:
@@ -47,5 +66,10 @@ def main():
             lista = adicionar(lista, jogador)
         elif opc == 2:
             mostrar(lista)
+        elif opc == 3:
+            jogador = input("qual o nome do jogador? ")
+            lista = final(lista, jogador)
+        elif opc == 4:
+            atual = percorrer(atual, jogador)
 
 main()
